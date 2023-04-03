f7ce3a924612210a520301daa636d403e94a5318
Rohit-singh19 <rohitsingh19032001@gmail.com>
2023-04-03 10:39:41 +0530

    navigation stack started

--- src/Routes/StackNavigator.js
+++ src/Routes/StackNavigator.js
@@ -9,159 +9,175 @@ import HeaderLeft from '../Components/HeaderLeft';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import ContactListScreen from '../Screens/ContactListScreen';
+import LoginScreen from '../Screens/LoginScreen';
+import WelcomeScreen from '../Screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 function StackNavigator() {
+  let isLoggedIn = false;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: COLORS.primaryGreen,
-          },
-          headerTintColor: COLORS.secondary,
-          title: 'WhatsApp',
-          headerTransparent: false,
+      {!isLoggedIn ? (
+        // not loggedIn
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="welcome" component={WelcomeScreen} />
+          <Stack.Screen name="login" component={LoginScreen} />
+        </Stack.Navigator>
+      ) : (
+        // on LogIn
+        <Stack.Navigator
+          screenOptions={{
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: COLORS.primaryGreen,
+            },
+            headerTintColor: COLORS.secondary,
+            title: 'WhatsApp',
+            headerTransparent: false,
 
-          // for search Icon
-          // headerSearchBarOptions: {
-          //   onChangeText: event => console.log(event.nativeEvent.text),
-          //   placeholder: 'search...',
-          //   textColor: COLORS.secondary,
-          //   headerIconColor: COLORS.secondary,
-          //   hintTextColor: COLORS.secondary,
-          //   shouldShowHintSearchIcon: false,
-          // },
-          // for screen animation
-          animation: 'fade_from_bottom',
-          headerRight: () => {
-            return (
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Ionicons
-                  size={27}
-                  color={COLORS.secondary}
+            // for search Icon
+            // headerSearchBarOptions: {
+            //   onChangeText: event => console.log(event.nativeEvent.text),
+            //   placeholder: 'search...',
+            //   textColor: COLORS.secondary,
+            //   headerIconColor: COLORS.secondary,
+            //   hintTextColor: COLORS.secondary,
+            //   shouldShowHintSearchIcon: false,
+            // },
+            // for screen animation
+            animation: 'fade_from_bottom',
+            headerRight: () => {
+              return (
+                <View
                   style={{
-                    marginRight: 10,
-                  }}
-                  name="camera-outline"
-                />
-                <MaterialIcons
-                  size={25}
-                  color={COLORS.secondary}
-                  style={{
-                    marginRight: 10,
-                  }}
-                  name="search"
-                />
-                <MaterialIcons
-                  size={25}
-                  color={COLORS.secondary}
-                  style={{
-                    marginRight: 10,
-                  }}
-                  name="more-vert"
-                />
-              </View>
-            );
-          },
-        }}>
-        <Stack.Screen name="Home" component={MainTabNavigator} />
-        <Stack.Screen
-          options={({navigation, route: {params}}) => {
-            const {user, lastMessage} = params;
-            return {
-              headerBackTitleVisible: true,
-              headerTitle: () => <HeaderLeft {...user} {...lastMessage} />,
-              headerRight: () => {
-                return (
-                  <View
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Ionicons
+                    size={27}
+                    color={COLORS.secondary}
                     style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}>
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
+                      marginRight: 10,
+                    }}
+                    name="camera-outline"
+                  />
+                  <MaterialIcons
+                    size={25}
+                    color={COLORS.secondary}
+                    style={{
+                      marginRight: 10,
+                    }}
+                    name="search"
+                  />
+                  <MaterialIcons
+                    size={25}
+                    color={COLORS.secondary}
+                    style={{
+                      marginRight: 10,
+                    }}
+                    name="more-vert"
+                  />
+                </View>
+              );
+            },
+          }}>
+          <Stack.Screen name="Home" component={MainTabNavigator} />
+          <Stack.Screen
+            options={({navigation, route: {params}}) => {
+              const {user, lastMessage} = params;
+              return {
+                headerBackTitleVisible: true,
+                headerTitle: () => <HeaderLeft {...user} {...lastMessage} />,
+                headerRight: () => {
+                  return (
+                    <View
                       style={{
-                        marginRight: 15,
-                      }}
-                      name="videocam"
-                    />
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
-                      style={{
-                        marginRight: 15,
-                      }}
-                      name="call"
-                    />
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
-                      name="more-vert"
-                    />
-                  </View>
-                );
-              },
-            };
-          }}
-          name="Chat"
-          component={ChatScreen}
-        />
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}>
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        style={{
+                          marginRight: 15,
+                        }}
+                        name="videocam"
+                      />
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        style={{
+                          marginRight: 15,
+                        }}
+                        name="call"
+                      />
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        name="more-vert"
+                      />
+                    </View>
+                  );
+                },
+              };
+            }}
+            name="Chat"
+            component={ChatScreen}
+          />
 
-        <Stack.Screen
-          options={({navigation, route: {params}}) => {
-            return {
-              headerBackTitleVisible: true,
-              headerTitle: () => (
-                <HeaderLeft name={'Select contact'} createdAt={new Date()} />
-              ),
-              headerRight: () => {
-                return (
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}>
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
+          <Stack.Screen
+            options={({navigation, route: {params}}) => {
+              return {
+                headerBackTitleVisible: true,
+                headerTitle: () => (
+                  <HeaderLeft name={'Select contact'} createdAt={new Date()} />
+                ),
+                headerRight: () => {
+                  return (
+                    <View
                       style={{
-                        marginRight: 15,
-                      }}
-                      name="videocam"
-                    />
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
-                      style={{
-                        marginRight: 15,
-                      }}
-                      name="call"
-                    />
-                    <MaterialIcons
-                      size={25}
-                      color={COLORS.secondary}
-                      name="more-vert"
-                    />
-                  </View>
-                );
-              },
-            };
-          }}
-          name="Allcontact"
-          component={ContactListScreen}
-        />
-      </Stack.Navigator>
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}>
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        style={{
+                          marginRight: 15,
+                        }}
+                        name="videocam"
+                      />
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        style={{
+                          marginRight: 15,
+                        }}
+                        name="call"
+                      />
+                      <MaterialIcons
+                        size={25}
+                        color={COLORS.secondary}
+                        name="more-vert"
+                      />
+                    </View>
+                  );
+                },
+              };
+            }}
+            name="Allcontact"
+            component={ContactListScreen}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
